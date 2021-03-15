477e61d1ddafdfb852aed041bc55d472a6fef31f
Ivan Dolgov <ivandolgov@setyl.com>
2021-03-16 00:35:04 +0300

    Add libs

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable global-require */
 const path = require('path');
 
 module.exports = {
@@ -5,5 +7,42 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // inject CSS to page
+            loader: 'style-loader',
+          },
+          {
+            // translates CSS into CommonJS modules
+            loader: 'css-loader',
+          },
+          {
+            // Run postcss actions
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    'autoprefixer',
+                    {
+                      // Options
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+          {
+            // compiles Sass to CSS
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
   },
 };
