9c3119791c10a4451826f84b8aae0d3c3b17dddf
johanla0 <idlhero@gmail.com>
2021-03-16 21:56:02 +0300

    Update to set the deploy [skip-ci]

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
   },
   module: {
     rules: [
