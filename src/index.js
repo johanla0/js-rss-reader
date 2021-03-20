8f27a75bfa72f218538e03876ffa05e65d108e6f
johanla0 <idlhero@gmail.com>
2021-03-21 00:34:55 +0300

    Add validation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-import '../scss/style.scss';
+import './scss/style.scss';
+import * as yup from 'yup';
+
+const urlSchema = yup.string().required().url();
+
+const form = document.querySelector('.rss-form');
+const url = document.querySelector('input[name="url"]');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  urlSchema.isValid(url.value)
+    .then((valid) => {
+      if (!valid) {
+        url.classList.add('is-invalid');
+      } else {
+        url.classList.remove('is-invalid');
+        url.classList.add('is-valid');
+      }
+    });
+});
+
+const example = document.querySelector('a#example');
+example.addEventListener('click', (e) => {
+  e.preventDefault();
+  url.value = example.textContent;
+});
