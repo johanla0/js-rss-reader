146fc7bb5d0e701cadfa0eed182266ff607d583f
johanla0 <idlhero@gmail.com>
2021-03-21 14:28:32 +0300

    Add watched state (on-change)

--- src/index.js
+++ src/index.js
@@ -1,46 +1,64 @@
 import './scss/style.scss';
 import * as yup from 'yup';
+import onChange from 'on-change';
 
-const render = (state, e) => {
+const urlSchema = yup.string().required().url();
+
+const render = (state, path) => {
   const url = document.querySelector('input[name="url"]');
-  if (e.type === 'submit') {
-    if (state.isValid) {
+  if (path === 'isValid') {
+    if (state.isValid === true) {
       url.classList.remove('is-invalid');
       url.classList.add('is-valid');
-    } else {
+    } else if (state.isValid === false) {
       url.classList.remove('is-valid');
       url.classList.add('is-invalid');
+    } else {
+      url.classList.remove('is-valid');
+      url.classList.remove('is-invalid');
     }
   }
   url.value = state.url;
 };
 
-const urlSchema = yup.string().required().url();
+const state = {
+  feeds: [],
+  posts: [],
+  errors: [],
+  isValid: '',
+  url: '',
+};
+
+const watchedState = onChange(state, (path, value, previousValue, name) => {
+  const temp = {
+    path,
+    value,
+    previousValue,
+    name,
+  };
+  console.log(temp);
+  render(state, path);
+});
 
 export default () => {
-  const state = {
-    feeds: [],
-    posts: [],
-    errors: [],
-    isValid: true,
-    url: '',
-  };
   const form = document.querySelector('.rss-form');
   const url = document.querySelector('input[name="url"]');
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    state.url = url.value;
+    watchedState.url = url.value;
     urlSchema.isValid(url.value)
       .then((valid) => {
-        state.isValid = valid;
-        render(state, e);
+        watchedState.isValid = valid;
       });
+  });
+
+  url.addEventListener('focus', () => {
+    watchedState.isValid = '';
   });
 
   const example = document.querySelector('a#example');
   example.addEventListener('click', (e) => {
     e.preventDefault();
-    state.url = example.textContent;
-    render(state, e);
+    watchedState.url = example.textContent;
   });
 };
